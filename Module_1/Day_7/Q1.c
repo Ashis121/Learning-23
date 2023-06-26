3b32c240672cafe5c11392f8b587f27085f14b91
Ashis121 <nayakashis121@gmail.com>
2023-06-26 16:33:14 +0530

    Day_7 completed

---
+++ Module_1/Day_7/Q1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    FILE *source, *destination;
+    char ch;
+
+    source = fopen("source_file.txt", "r");
+    if (source == NULL) {
+        printf("Unable to open the source file.\n");
+        return 1;
+    }
+    destination = fopen("destination_file.txt", "w");
+    if (destination == NULL) {
+        printf("Unable to create the destination file.\n");
+        fclose(source);
+        return 1;
+    }
+    while ((ch = fgetc(source)) != EOF) {
+        fputc(ch, destination);
+    }
+
+    // Close the files
+    fclose(source);
+    fclose(destination);
+
+    printf("File copied successfully.\n");
+
+    return 0;
+}
